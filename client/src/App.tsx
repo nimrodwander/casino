8da38786b0d040c5f4c85a5244638eb1f46bde83
nimrod <nimrodw@kubiyax.com>
2026-02-05 20:30:57 +0200

    cented the main content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Container, Box } from '@mui/material';
 import { Header } from './components/Header';
 import { SlotMachine } from './components/SlotMachine';
 import { Footer } from './components/Footer';
 
 export const App: React.FC = () => {
   return (
-    <>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Header />
-      <Container maxWidth="sm" sx={{ py: 4, pb: 8 }}>
+      <Container
+        maxWidth="sm"
+        sx={{
+          flexGrow: 1,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          pb: 8,
+        }}
+      >
         <SlotMachine />
       </Container>
       <Footer />
-    </>
+    </Box>
   );
 };
