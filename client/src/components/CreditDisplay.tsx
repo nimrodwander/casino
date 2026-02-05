fa9b0539dd3adf539a779ebbf3698df8a20cc5e6
nimrod <nimrodw@kubiyax.com>
2026-02-05 20:47:03 +0200

    changed something

--- client/src/components/CreditDisplay.tsx
+++ client/src/components/CreditDisplay.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Chip } from '@mui/material';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 
 interface CreditDisplayProps {
   credits: number;
@@ -8,6 +9,7 @@ interface CreditDisplayProps {
 export const CreditDisplay: React.FC<CreditDisplayProps> = ({ credits }) => {
   return (
     <Chip
+      icon={<MonetizationOnIcon sx={{ color: 'secondary.main', fontSize: 18 }} />}
       label={`Credits ${credits}`}
       sx={{
         bgcolor: '#1e3a5f',
