7ad36a0e8886b16465e1622f591562717ad6f2a4
nimrod <nimrodw@kubiyax.com>
2026-02-06 13:29:13 +0200

    changed varaible name

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { AppBar, Toolbar, Stack, Typography } from '@mui/material';
 import { ALL_SYMBOLS, SYMBOL_NAMES, SYMBOL_REWARDS } from '@casino/shared';
+import { AppBar, Stack, Toolbar, Typography } from '@mui/material';
+import React from 'react';
 
 export const Footer: React.FC = () => (
   <AppBar position="fixed" color="default" sx={{ top: 'auto', bottom: 0 }}>
     <Toolbar variant="dense" sx={{ justifyContent: 'center' }}>
       <Stack direction="row" spacing={3}>
-        {ALL_SYMBOLS.map((s) => (
-          <Stack key={s} direction="row" alignItems="center" spacing={0.5}>
+        {ALL_SYMBOLS.map((symbol) => (
+          <Stack key={symbol} direction="row" alignItems="center" spacing={0.5}>
             <Typography variant="caption" sx={{ fontWeight: 'bold' }}>
-              {s}
+              {symbol}
             </Typography>
             <Typography variant="caption" sx={{ color: 'text.secondary' }}>
-              {SYMBOL_NAMES[s]}
+              {SYMBOL_NAMES[symbol]}
             </Typography>
             <Typography variant="caption" sx={{ fontWeight: 'bold', color: 'success.main' }}>
-              +{SYMBOL_REWARDS[s]}
+              +{SYMBOL_REWARDS[symbol]}
             </Typography>
           </Stack>
         ))}
