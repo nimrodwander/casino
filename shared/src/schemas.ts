278a7e0c1c9d690f0d31857fe58fc14609f673d7
nimrod <nimrodw@kubiyax.com>
2026-02-07 22:00:47 +0200

    added some comments

--- shared/src/schemas.ts
+++ shared/src/schemas.ts
@@ -32,12 +32,17 @@ export const responseSchema = z.object({
   message: z.string().optional(),
 });
 
-// Inferred types
+//Requests
 export type CreateSessionRequest = z.infer<typeof createSessionRequestSchema>;
-export type CreateSessionData = z.infer<typeof createSessionDataSchema>;
-export type RollData = z.infer<typeof rollDataSchema>;
-export type CashOutData = z.infer<typeof cashOutDataSchema>;
+
+//Response generic schema
 export type Response<T = unknown> = {
   data: T;
   message?: string;
 };
+
+//Responses enveloped data
+export type CreateSessionData = z.infer<typeof createSessionDataSchema>;
+export type RollData = z.infer<typeof rollDataSchema>;
+export type CashOutData = z.infer<typeof cashOutDataSchema>;
+
