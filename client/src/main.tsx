8f1e81a5474272d38858ab33f14a6dd7745de3c5
nimrod <nimrodw@kubiyax.com>
2026-02-08 10:24:00 +0200

    added theme config file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import { App } from './App';
-
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: { main: '#e94560' },
-    secondary: { main: '#ffd700' },
-    success: { main: '#4ecca3' },
-    background: { default: '#1a1a2e', paper: '#16213e' },
-  },
-});
+import { theme } from './theme.config';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ client/src/theme.config.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: { main: '#e94560' },
+    secondary: { main: '#ffd700' },
+    success: { main: '#4ecca3' },
+    background: { default: '#1a1a2e', paper: '#16213e' },
+  },
+});
